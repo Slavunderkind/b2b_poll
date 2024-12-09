ea5865e37c2815dc6d3895955161a3c1c4d7b450
Slavka Tsankova <smcankova@gmail.com>
2024-12-09 16:09:55 +0200

    Add changes in sidekiq config to be able to test properly emailing on dev mode

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -86,7 +86,7 @@ Rails.application.configure do
   else
     config.action_mailer.delivery_method = :letter_opener
     config.action_mailer.perform_deliveries = true
-    config.action_mailer.letter_opener_settings = { location: Rails.root.join('tmp', 'letter_opener') }
+    # config.action_mailer.letter_opener_settings = { location: Rails.root.join('tmp', 'letter_opener') }
     config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   end
 end
